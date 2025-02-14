ff1db4546e43da0b595d1914567e9c049ba71c26
munania <munaniadeno@gmail.com>
2025-02-14 19:11:33 +0300

    First Commit

--- lib/services/data_source.dart
+++ lib/services/data_source.dart
@@ -21,37 +21,4 @@ class CountryService {
       throw Exception('Failed to load countries: $e');
     }
   }
-
-  Future<Country?> getCountryByCode(String code) async {
-    try {
-      final response = await http.get(Uri.parse('$baseUrl/alpha/$code'));
-
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonData = json.decode(response.body);
-        if (jsonData.isNotEmpty) {
-          return Country.fromJson(jsonData.first);
-        }
-        return null;
-      } else {
-        throw Exception('Failed to load country: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to load country: $e');
-    }
-  }
-
-  Future<List<Country>> searchCountries(String name) async {
-    try {
-      final response = await http.get(Uri.parse('$baseUrl/name/$name'));
-
-      if (response.statusCode == 200) {
-        final List<dynamic> jsonData = json.decode(response.body);
-        return jsonData.map((json) => Country.fromJson(json)).toList();
-      } else {
-        throw Exception('Failed to search countries: ${response.statusCode}');
-      }
-    } catch (e) {
-      throw Exception('Failed to search countries: $e');
-    }
-  }
 }
